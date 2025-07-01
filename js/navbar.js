666817d74ecc8a074d8a7b8720af082b0479fc9c
Lucas Hubner <160272134+LUCASGOODPROGRAMMER@users.noreply.github.com>
2025-06-30 20:23:35 -0400

    simulação de menu de admin

--- js/navbar.js
+++ js/navbar.js
@@ -1,18 +1,40 @@
-const btnMenu = document.querySelector("#btn-menu")
-const menu = document.querySelector("#menu")
+const btnMenu = document.querySelector("#btn-menu");
+const menu = document.querySelector("#menu");
 
 btnMenu.addEventListener("click", (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    if(menu.style.display === 'none'){
-         menu.style.display = 'flex'
-         return;
+    const isHidden = getComputedStyle(menu).display === 'none';
+
+    if (isHidden) {
+        menu.style.display = 'block';
+        menu.classList.remove("fade-out");
+        menu.classList.add("fade-in");
+
+        setTimeout(() => {
+            menu.classList.remove("fade-in");
+        }, 500);
+
+    } else {
+        menu.classList.remove("fade-in");
+        menu.classList.add("fade-out");
+
+        setTimeout(() => {
+            menu.classList.remove("fade-out");
+            menu.style.display = 'none';
+        }, 500);
     }
-    menu.classList.add("fade-out")
-    setTimeout(() => {
-        menu.classList.remove("fade-out")
-        menu.style.display = 'none'
-    }, 500)
-    
-})
+});
 
+const btnUser = document.querySelectorAll("#btn-admin, #config");
+const formLogin = document.querySelector("#form-login");
+
+btnUser.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        if (formLogin.style.display === 'none' || formLogin.style.display === '') {
+            formLogin.style.display = 'flex';
+        } else {
+            formLogin.style.display = 'none';
+        }
+    });
+});
