3b3f78ab07c5e0e126319ea6fd7d3a89c5c353d5
LUCASGOODPROGRAMMER <lucashubner2019@gmail.com>
2025-07-14 17:09:52 -0400

    restruturação

--- js/tela_principal.js
+++ js/tela_principal.js
@@ -1,25 +1,2 @@
-
-const btnView = document.querySelectorAll(".btn-view")
-const btnTrash = document.querySelectorAll(".btn-trash")
-const btnConfig = document.querySelectorAll(".btn-edit")
-
-const btnAdd = document.querySelector("#btn-new");
-
-btnAdd.addEventListener("click", () => {
-window.location.href = "forms/html/form1.html"
-});
-
-const gridBox = document.querySelector("#grid-box");
-
-gridBox.addEventListener("click", (event) => {
-  const btnView = event.target.closest(".btn-view");
-  if (btnView) {
-    const boxGame = btnView.closest(".box-game");
-    if (boxGame) {
-      boxGame.classList.toggle("dimmed");
-    }
-  }
-});
-
-
-
+const btnMenu = document.querySelector("#btn-menu")
+const btnLogar = document.querySelector("#btn-logar")
