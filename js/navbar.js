3329c35ed3f656f64c65e322162991b490020e23
LUCASGOODPROGRAMMER <lucashubner2019@gmail.com>
2025-07-16 19:08:45 -0400

    agora o eventos está realmente desaparecendo

--- js/navbar.js
+++ js/navbar.js
@@ -1,24 +1,55 @@
 // Função para inicializar os eventos da navbar e do menu dinâmico
 function initNavbarEvents() {
     // Seletores dos elementos do header
-    const btnMenu = document.querySelector("#btn-menu");
     const btnLogar = document.querySelector("#btn-logar");
-    const menu = document.querySelector("#menu");
+    const btnLogin = document.querySelector("#btn-login");
     const form = document.querySelector("#form-login");
+    const btnMenu = document.querySelector("#btn-menu");
+    const menu = document.querySelector("#menu");
 
-    // Garante que todas as divs com id menu estejam ocultas inicialmente
-    document.querySelectorAll('div#menu:not(.hide)').forEach(div => {
-        div.classList.add('hide');
-    });
+    // Função para alternar display flex do form-login
+    function toggleFormLogin() {
+        if (form) {
+            if (form.style.display === "flex") {
+                form.style.display = "none";
+            } else {
+                form.style.display = "flex";
+            }
+        }
+    }
 
-    // Evento para desbloquear opções de admin após login
+    // Função para alternar display flex do menu
+    function toggleMenu() {
+        if (menu) {
+            if (menu.style.display === "flex") {
+                menu.style.display = "none";
+            } else {
+                menu.style.display = "flex";
+            }
+        }
+    }
+
+    // Evento para btn-logar
+    if (btnLogar) {
+        btnLogar.addEventListener('click', toggleFormLogin);
+    }
+    // Evento para btn-login (caso exista)
+    if (btnLogin) {
+        btnLogin.addEventListener('click', toggleFormLogin);
+    }
+    // Evento para btn-menu
+    if (btnMenu) {
+        btnMenu.addEventListener('click', toggleMenu);
+    }
+
+    // Evento para submit-login revelar o gerenciador
     const submitLogin = document.querySelector('#submit-login');
     if (submitLogin) {
         submitLogin.addEventListener('click', function() {
-            const optionsHideDiv = document.querySelector('#menu .options.hide');
-            if (optionsHideDiv) {
-                optionsHideDiv.classList.toggle('hide');
-                alert('Opções de administrador desbloqueadas!');
+            const gerenciadorOption = document.querySelector('#menu .options.hide a[href="gerenciador.html"]')?.parentElement;
+            if (gerenciadorOption) {
+                gerenciadorOption.classList.remove('hide');
+                alert('Opção GERENCIADOR desbloqueada!');
             }
         });
     }
